969c89253025262133d71ff812cd6779c192ed37
Jen Lu <jennifer.lu717@gmail.com>
2023-09-20 15:54:42 -0400

    Update kraken_processing.h

--- src/kraken_processing.h
+++ src/kraken_processing.h
@@ -1,6 +1,6 @@
 /*********************************************************************
  * kraken_processing.h is used as part of the kmer2distr script
- * Copyright (C) 2016-2022 Jennifer Lu, jlu26@jhmi.edu
+ * Copyright (C) 2016-2023 Jennifer Lu, jlu26@jhmi.edu
  *
  * This file is part of Bracken.
  * Bracken is free software; you can redistribute it and/or modify
